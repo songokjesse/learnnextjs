6a92238380a053db2b98c9907b8cfaf723cc665e
jessesongok <jessesongok@gmail.com>
2021-02-11 14:16:12 +0300

    Added the 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+const notFound = () => {
+    return ( 
+        <div>
+
+            <h1>404 Oooops ........</h1>
+            <p>Page your are looking for is not found</p>
+        </div>
+     );
+}
+ 
+export default notFound;
